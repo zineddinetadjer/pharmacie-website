c40f861b1cb1a8e15b1c920cc2cbe818c600d210
zineddinetadjer <zinedinetadjer1@gmail.com>
2022-09-26 19:41:05 +0100

    adding the images file and start working on the navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../imzges/agieu-removebg-preview.png";
+
+function Navbar() {
+  return (
+    <div>
+      <img src={logo} alt="LOGO" />
+    </div>
+  );
+}
+
+export default Navbar;
