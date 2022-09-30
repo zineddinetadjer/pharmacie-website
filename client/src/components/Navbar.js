9cbc5b91a871d203d282c2dab1818ed4ed72df3a
zineddinetadjer <zinedinetadjer1@gmail.com>
2022-09-30 21:50:48 +0100

    commit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,17 @@
 import React from "react";
-import logo from "../imzges/agieu-removebg-preview.png";
+import logo from "../images/agieu-removebg-preview.png";
+import "../styles/navbar.css";
 
 function Navbar() {
   return (
-    <div>
-      <img src={logo} alt="LOGO" />
+    <div className="navbar">
+      <div className="logo">
+        <img src={logo} alt="LOGO" />
+      </div>
+      <div className="navbar1">
+        <div className="search"></div>
+        <div className="links"></div>
+      </div>
     </div>
   );
 }
